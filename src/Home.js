64e126855e2611fc4d45cfaecb8f89772787749d
Viktoriia Petrova <etrovavica1221@gmail.com>
2020-11-03 22:40:28 +0000

    Add responsive design for profile

--- src/Home.js
+++ src/Home.js
@@ -1,31 +1,32 @@
 import React from 'react';
 import { Card, CardImg, CardTitle, CardText} from 'reactstrap';
+
 import SocialMobile from './SocialMobile.js';
 import './styles/Home.css';
 
 function Home() {
   const avatar = require('./styles/avatar.jpg');
 
-    return (
-      <div>
-        <Card id="aboutMe-container">
-          <CardImg id="profile-wrap" src={avatar} alt="profile">
-          </CardImg>
-          <div id="my-info">
-            <CardTitle id="greeting">
-              Hi, I am Viktoriia Petrova!
-            </CardTitle>
-            <CardText>
-              An ambitious full-stack software developer and a graduate of<span> </span>
-              <a href="https://www.manchestercodes.com" target="_blank" rel="noopener noreferrer">
-                  Manchester Codes
-              </a>.
-            </CardText>
-          </div>
-        </Card> 
-        <SocialMobile /> 
-      </div>
-    );
+  return (
+    <div>
+      <Card id="aboutMe-container">
+        <CardImg id="profile-wrap" src={avatar} alt="profile">
+        </CardImg>
+        <div id="my-info">
+          <CardTitle id="greeting">
+            Hi, I am Viktoriia Petrova!
+          </CardTitle>
+          <CardText>
+            An ambitious full-stack software developer and a graduate of<span> </span>
+            <a href="https://www.manchestercodes.com" target="_blank" rel="noopener noreferrer">
+                Manchester Codes
+            </a>.
+          </CardText>
+        </div>
+      </Card> 
+      <SocialMobile /> 
+    </div>   
+  );
 }
   
 export default Home;
