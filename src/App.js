8d15ba98e07f46a6e142da9359e033b9df7ab10a
Viktoriia Petrova <etrovavica1221@gmail.com>
2021-06-04 10:44:56 +0100

    Add background-color: black

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { Route, Switch, BrowserRouter } from 'react-router-dom';
-import './styles/App.css';
-import NavBar from './Navbar.js';
-import Home from './Home.js';
-import Project from './Project.js';
-import Social from './Social.js';
-import Contact from './Contact.js';
+import React from "react";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
+import "./styles/App.css";
+import NavBar from "./Navbar.js";
+import Home from "./Home.js";
+import Project from "./Project.js";
+import Social from "./Social.js";
+import Contact from "./Contact.js";
 
 function App() {
   return (
@@ -13,11 +13,11 @@ function App() {
       <div className="App">
         <NavBar />
         <Social />
-          <Switch>
-            <Route exact path="/" render={() => <Home />} />
-            <Route exact path="/projects" render={() => <Project />} />
-            <Route exact path="/contact" render={() => <Contact />}/>
-          </Switch>
+        <Switch>
+          <Route exact path="/" render={() => <Home />} />
+          <Route exact path="/projects" render={() => <Project />} />
+          <Route exact path="/contact" render={() => <Contact />} />
+        </Switch>
       </div>
     </BrowserRouter>
   );
